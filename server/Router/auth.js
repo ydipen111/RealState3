cb6273b9a9f34eedc01f49bb030c9bbe3d05970d
dipen <aggressivebanda74@gmail.com>
2024-08-23 08:31:21 +0545

    commiting it

--- server/Router/auth.js
+++ server/Router/auth.js
@@ -6,5 +6,7 @@ import * as auth from "../controller/authCon.js";
 
 //get, post put delete
 router.get('/', auth.welcome)
+
+router.post('/pre-register', auth.preRegister); //
 export default router;
 
--- server/controller/authCon.js
+++ server/controller/authCon.js
@@ -3,3 +3,22 @@ export const welcome = (req, res) => {
     data: "hello i am talking from auth controller"
   })
 }
+export const preRegister = async (req, res) => {
+  try {
+    //create jwt with email and passowrt then email as clickable link
+    //only when user click on that email link, registration competes
+    console.log(req.body);
+
+    const emailSent = true;
+    if (emailSent) {
+      return res.json({ ok: true });
+    } else {
+      return res.json({ ok: false });
+    }
+
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: "somethin went wrong. Try again" })
+
+  }
+}
